ee6cf4e45cc44d03bca542c6549b80fd63bf9314
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-01-21 13:00:12 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
 
-  #Users
+  #Users  
+  resources :users
   get '/signup', to: 'users#new'
-
+  post '/signup', to: 'users#create'
 
 end
