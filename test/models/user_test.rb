dbac905161642bcb28f9ebf13af4a48cd33ef1b4
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-01-31 18:52:39 +0100

    Implement advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -70,7 +70,11 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a length of more than 6" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
-  end 
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 end
 
