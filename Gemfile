e1ed213318b98554275cadde6d4057f4ad027169
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-02-04 23:18:12 +0100

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -12,6 +12,11 @@ gem 'rails', '5.1.4'
 gem 'bootstrap-sass', '3.3.7'
 # Password encryption 
 gem 'bcrypt', '3.1.11'
+# Generate random users
+gem 'faker', '1.7.3'
+# Pagination
+gem 'will_paginate',           '3.1.6'
+gem 'bootstrap-will_paginate', '1.0.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,5 @@
 module SessionsHelper
+
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -37,6 +38,29 @@ module SessionsHelper
     @current_user = nil
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
+  #Redirects to the store location (or to the default)
+  def redirect_back_or(default)
+    redirect_to (session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  #Stores the URL trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end 
+
 end
+
+
+
+
+
+
+
+
 
 
