1f0716549e407c2911de0c640a9e6d4b3209bd58
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-02-06 23:02:39 +0100

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
 
-  attr_accessor :remember_token
-
-  before_save {self.email.downcase!}
-
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: {maximum: 50}
 
@@ -18,13 +17,13 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # Returns a random token.
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
@@ -35,13 +34,36 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  #Activates an account
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  #Sends an activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  #PRIVATE
+  private 
+
+  def downcase_email
+    self.email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end
