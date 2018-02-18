9005b60516bb5f09220506944b2614fff89410a8
Alexandre Ruban <alexandre.ruban@gmail.com>
2018-02-18 16:38:09 +0100

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,26 @@
 class User < ApplicationRecord
+
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :followed_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
+
 
   attr_accessor :remember_token, :activation_token, :reset_token
+
   before_save :downcase_email
   before_create :create_activation_digest
 
-  validates :name, presence: true, length: {maximum: 50}
-
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, 
-  presence: true, 
-  length: {maximum: 250}, 
-  format: {with: VALID_EMAIL_REGEX},
-  uniqueness: {case_sensitive: false}
-
 
   has_secure_password
+
+  validates :name, presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 250}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
+  # Crypts a string using Bcrypt gem
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
@@ -69,7 +72,22 @@ class User < ApplicationRecord
   end
 
   def feed
-    microposts
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+  end
+
+  # Relationships functions
+  
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   #PRIVATE
